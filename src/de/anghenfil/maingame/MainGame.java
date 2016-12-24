8b6b7a4c35b7a064678eb12edd869cad86b70ab1
anghenfil <anghenfil@anghofus>
2016-12-24 15:05:52 +0100

    Fixed Bug

--- src/de/anghenfil/maingame/MainGame.java
+++ src/de/anghenfil/maingame/MainGame.java
@@ -4,21 +4,22 @@ import java.awt.EventQueue;
 
 import de.anghenfil.gui.Window;
 import de.anghenfil.room.Room;
+import de.anghenfil.textdesign.TD;
 
 public class MainGame {
 	static Room room;
 	static Window window;
+	static User user;
 	public static void play(){
 		EventQueue.invokeLater(new Runnable() { //Start GUI
 			public void run() {
 				try {
 					window = new Window();
 					window.initialize();
-					User user = UserManager.loadUser();
+					user = UserManager.loadUser();
 					room = new Room();
 					room = room.loadRoom(user.getAct_room()); //Load last visited Room from User object
-					
-					window.addText(room.getRoomDescription()); //Show actual room Description
+					TD.description(room.getRoomDescription()); //Show actual room Description
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -28,8 +29,14 @@ public class MainGame {
 	public static Room getRoom(){
 		return room;
 	}
+	public static void setRoom(Room room1){
+		room = room1;
+	}
 	public static Window getWindow(){
 		return window;
+	}
+	public static User getUser(){
+		return user;
 	}
 	
 }
