4b2886c993625a62fd65034e143fd6364a1b4a6a
anghenfil <anghenfil@anghofus>
2016-12-25 20:14:55 +0100

    Fixed an Error

--- src/de/anghenfil/textdesign/TD.java
+++ src/de/anghenfil/textdesign/TD.java
@@ -6,14 +6,16 @@ public class TD {
 		de.anghenfil.gui.Window window = MainGame.getWindow();
 		int countchars;
 		String line = "";
-
-		countchars = content.length();
-		for(int i = 0;i<countchars+2;i++){
-			line = line.concat("=");
+		try{
+			countchars = content.length();
+			for(int i = 0;i<countchars+2;i++){
+				line = line.concat("=");
+			}
+			window.addText("<b>"+ line + "</b>");
+			window.addText("<b>"+content+"</b>");
+			window.addText("<b>"+ line + "</b>");
+		}catch(NullPointerException e){
 		}
-		window.addText("<b>"+ line + "</b>");
-		window.addText("<b>"+content+"</b>");
-		window.addText("<b>"+ line + "</b>");
 	}public static void input_question(String content){
 		de.anghenfil.gui.Window window = MainGame.getWindow();
 		window.addText("|| ");
@@ -22,11 +24,14 @@ public class TD {
 		de.anghenfil.gui.Window window = MainGame.getWindow();
 		window.addText("> " + content);
 	}public static void description(String content){
-		de.anghenfil.gui.Window window = MainGame.getWindow();
-		window.addText("<b>"+content+"</b>");
+		try{
+			de.anghenfil.gui.Window window = MainGame.getWindow();
+			window.addText(content);
+		}catch(NullPointerException e){
+		}
 	}public static void error(String content){
-		de.anghenfil.gui.Window window = MainGame.getWindow();
-		window.addText("<b>"+content+"</b>");
+			de.anghenfil.gui.Window window = MainGame.getWindow();
+			window.addText("<b>"+content+"</b>");
 	}public static void error(){
 		error("Deine Antwort konnte nicht erkannt werden. Bitte wiederhole deine Eingabe!");
 	}
