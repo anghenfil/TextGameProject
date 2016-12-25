1137973cadf45e3fc686005bb36a4382dd1a6ced
anghenfil <anghenfil@anghofus>
2016-12-25 20:14:26 +0100

    Save user data every time anything changed

--- src/de/anghenfil/user/User.java
+++ src/de/anghenfil/user/User.java
@@ -34,120 +34,242 @@ public class User implements Serializable{
 	}
 	public void setProfession(String profession) {
 		this.profession = profession;
+		try {
+			saveUser();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	public String getRace() {
 		return race;
+		
 	}
 	public void setRace(String race) {
 		this.race = race;
+		try {
+			saveUser();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	public int getHealthPoints() {
 		return healthPoints;
 	}
 	public void setHealthPoints(int healthPoints) {
 		this.healthPoints = healthPoints;
+		try {
+			saveUser();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	public int getBonusHealth() {
 		return bonusHealth;
 	}
 	public void setBonusHealth(int bonusHealth) {
 		this.bonusHealth = bonusHealth;
+		try {
+			saveUser();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	public int getActionPoints() {
 		return actionPoints;
 	}
 	public void setActionPoints(int actionPoints) {
 		this.actionPoints = actionPoints;
+		try {
+			saveUser();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	public int getBonusActionPoints() {
 		return bonusActionPoints;
 	}
 	public void setBonusActionPoints(int bonusActionPoints) {
 		this.bonusActionPoints = bonusActionPoints;
+		try {
+			saveUser();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	public int getActRoom() {
 		return actRoom;
 	}
 	public void setActRoom(int actRoom) {
 		this.actRoom = actRoom;
+		try {
+			saveUser();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	public int getSpeed() {
 		return speed;
 	}
 	public void setSpeed(int speed) {
 		this.speed = speed;
+		try {
+			saveUser();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	public int getBonusSpeed() {
 		return bonusSpeed;
+		
 	}
 	public void setBonusSpeed(int bonusSpeed) {
 		this.bonusSpeed = bonusSpeed;
+		try {
+			saveUser();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	public int getEloquence() {
 		return eloquence;
 	}
 	public void setEloquence(int eloquence) {
 		this.eloquence = eloquence;
+		try {
+			saveUser();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	public int getBonusEloquence() {
 		return bonusEloquence;
 	}
 	public void setBonusEloquence(int bonusEloquence) {
 		this.bonusEloquence = bonusEloquence;
+		try {
+			saveUser();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	public int getCharm() {
 		return charm;
 	}
 	public void setCharm(int charm) {
 		this.charm = charm;
+		try {
+			saveUser();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	public int getBonusCharm() {
 		return bonusCharm;
 	}
 	public void setBonusCharm(int bonusCharm) {
 		this.bonusCharm = bonusCharm;
+		try {
+			saveUser();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	public int getIntelligence() {
 		return intelligence;
 	}
 	public void setIntelligence(int intelligence) {
 		this.intelligence = intelligence;
+		try {
+			saveUser();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) { //TODO: Save user every time, something changed
 		this.name = name;
+		try {
+			saveUser();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	public int getBonus_health() {
 		return bonusHealth;
 	}
 	public void setBonus_health(int bonus_health) {
 		this.bonusHealth = bonus_health;
+		try {
+			saveUser();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	public int getBonus_ap() {
 		return bonusActionPoints;
 	}
 	public void setBonus_ap(int bonus_ap) {
 		this.bonusActionPoints = bonus_ap;
+		try {
+			saveUser();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	public int getHealth() {
 		return healthPoints;
 	}
 	public void setHealth(int health) {
 		this.healthPoints = health;
+		try {
+			saveUser();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	public int getAp() {
 		return actionPoints;
 	}
 	public void setAp(int ap) {
 		this.actionPoints = ap;
+		try {
+			saveUser();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	public int getAct_room() {
 		return actRoom;
 	}
 	public void setAct_room(int actRoom) {
 		this.actRoom = actRoom;
+		try {
+			saveUser();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	public void saveUser() throws IOException{
 		OutputStream savedata = null;
@@ -168,9 +290,21 @@ public class User implements Serializable{
 	}
 	public void addItem(int itemID){
 		this.items.add(itemID);
+		try {
+			saveUser();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	public void removeItem(int itemID){
 		this.items.remove(itemID);
+		try {
+			saveUser();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	public boolean hasBracket(int bracketID){
 		boolean contains;
@@ -183,6 +317,12 @@ public class User implements Serializable{
 	}
 	public void addBracket(int bracketID){
 		this.items.add(bracketID);
+		try {
+			saveUser();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 }
