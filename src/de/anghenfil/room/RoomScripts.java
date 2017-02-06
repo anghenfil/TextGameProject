a21675d7985c9c814fc7fa98a5d8dc071346d7c9
anghenfil <ares@anghenfil.de>
2017-02-06 13:52:12 +0100

    Fixed small errors.

--- src/de/anghenfil/room/RoomScripts.java
+++ src/de/anghenfil/room/RoomScripts.java
@@ -12,32 +12,23 @@ public class RoomScripts{
 		//ROOM 1
 		case 1:
 			if(input.length <= 1){
-				window.addText(Messages.getString("Error.whattoinspect")); //$NON-NLS-1$
-				//TODO: use error method
+				TD.error((Messages.getString("Error.whattoinspect"))); //$NON-NLS-1$
 			}else{
 				String string = input[1];
 				if (Messages.getString("RoomScript.Bookshelf").toLowerCase().equals(string) || Messages.getString("RoomScript.Bookshelfs").toLowerCase().equals(string)) {
 					window.addText(Messages.getString("RoomScript.Bookshelfdescription")); //$NON-NLS-1$
-				} else if (Messages.getString("RoomScript.pendulumclock").toLowerCase().equals(string)) {
+				} else if (Messages.getString("RoomScript.pendulumclock").toLowerCase().equals(string) || Messages.getString("RoomScript.clock").toLowerCase().equals(string)) {
 					window.addText(Messages.getString("RoomScript.pendulumclockdescription")); //$NON-NLS-1$
 				} else if (Messages.getString("RoomScript.desk").toLowerCase().equals(string)) {
 					window.addText(Messages.getString("RoomScript.deskdescription")); //$NON-NLS-1$
 				} else {
-					window.addText(Messages.getString("Error.cantinspect")); //$NON-NLS-1$
+					TD.error(Messages.getString("Error.cantinspect")); //$NON-NLS-1$
 				}
 			}
 			break;
 			
 		//ROOM 2
 		case 2:
-			if(input.length <= 1){
-				window.addText(Messages.getString("Error.whattoinspect")); //$NON-NLS-1$
-			}else{
-				switch(input[1]){
-					default:
-						window.addText(Messages.getString("RoomScript.cantinspect")); //$NON-NLS-1$
-				}
-			}	
 		}
 	}
 	public static void customCode(int roomID){
