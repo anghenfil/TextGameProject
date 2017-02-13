8916ade74a1f7d6659c0af90b3b63161115ae0a2
anghenfil <ares@anghenfil.de>
2017-02-13 13:33:30 +0100

    Removed unused variable.

--- src/de/anghenfil/sql/SqlTools.java
+++ src/de/anghenfil/sql/SqlTools.java
@@ -1,16 +1,13 @@
 package de.anghenfil.sql;
 
-import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class SqlTools {
 	public static Connection getConnection() throws Exception{
 		Connection c = null;
-		File path;
 		
 		Class.forName("org.sqlite.JDBC").newInstance();
-		path = new File("data/rooms.db");
 		c = DriverManager.getConnection("jdbc:sqlite::resource:rooms.db");
 		return c;
 	}
